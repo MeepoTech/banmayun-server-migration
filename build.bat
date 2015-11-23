ca9ee10b34fef6a28b2ff56ae7b517d5c2e75e22
Qiuping Wang <wangqiuping816@gmail.com>
2015-11-23 16:56:19 +0800

    2.0.0a47

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-call mvn versions:set -DnewVersion=2.0.0a46 -f ./pom.xml clean install
+call mvn versions:set -DnewVersion=2.0.0a47 -f ./pom.xml clean install
 call mvn -f ./pom.xml assembly:assembly
 copy .\target\*.jar .\
